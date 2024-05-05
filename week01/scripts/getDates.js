935d71628fed8d7ad7b11441db279aac2ea96604
sAiNtDVNTM <josedrago2011@hotmail.com>
2024-05-05 00:13:11 -0600

    Week 2 done

--- week01/scripts/getDates.js
+++ week01/scripts/getDates.js
@@ -1,7 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var currentYear = new Date().getFullYear();
-    document.getElementById("year").textContent = currentYear;
-    
-    var lastModifiedDate = document.lastModified;
-    document.getElementById("lastModified").textContent = "Last Modification: " + lastModifiedDate;
+  var currentYear = new Date().getFullYear();
+  document.getElementById("year").textContent = currentYear;
+
+  var lastModifiedDate = document.lastModified;
+  document.getElementById("lastModified").textContent = "Last Modification: " + lastModifiedDate;
+
+});
+
+ // Hamburger menu functionality
+ const hamburger = document.querySelector('#hamburger');
+ const menu = document.querySelector('#anime');
+
+ hamburger.addEventListener('click',  () => {
+    menu.classList.toggle('open');
+    hamburger.classList.toggle('open');
+ });
+
+ const darkModeButton = document.getElementById("darkModeButton");
+  const main = document.querySelector("main");
+
+  darkModeButton.addEventListener("click", function () {
+    main.classList.toggle("dark-mode");
   });
