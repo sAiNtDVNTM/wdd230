8b19961371b817072bc5b7cd7cd29dc125bdd27e
sAiNtDVNTM <josedrago2011@hotmail.com>
2024-05-11 21:36:16 -0600

    Lazy complete

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var currentYear = new Date().getFullYear();
+    document.getElementById("year").textContent = currentYear;
+
+    var lastModifiedDate = document.lastModified;
+    document.getElementById("lastModified").textContent =
+        "Last Modification: " + lastModifiedDate;
+});
