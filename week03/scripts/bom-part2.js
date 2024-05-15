08e19ad3c9b1e09b08e164fbf09bc981373b4cdc
sAiNtDVNTM <josedrago2011@hotmail.com>
2024-05-14 23:59:58 -0600

    week 3 done

---
+++ week03/scripts/bom-part2.js
@@ -0,0 +1,50 @@
+const inputElement = document.querySelector('#favchap');
+const buttonElement = document.querySelector('button');
+const listElement = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+buttonElement.addEventListener('click', () => {
+    if(inputElement.value != '') {
+        displayList(inputElement.value);
+        chaptersArray.push(inputElement.value);
+        setChapterList();
+        inputElement.value = '';
+        inputElement.focus();
+    }
+});
+
+function displayList (item) {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+    li.append(deleteButton);
+    listElement.append(li);
+    deleteButton.addEventListener('click', function () {
+        listElement.removeChild(li);
+        deleteChapter(li.textContent);
+        inputElement.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+function deleteChapter(chapter){
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+}
+function deleteChapter(chapter){
+    chapter = chapter.slice(0, chapter.length -1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
