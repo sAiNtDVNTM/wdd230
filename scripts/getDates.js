a0f9c139a97cde3375c338945d6ee539027f8b3f
sAiNtDVNTM <josedrago2011@hotmail.com>
2024-05-18 15:03:57 -0600

    Week03 done

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -22,3 +22,14 @@ document.addEventListener("DOMContentLoaded", function () {
   darkModeButton.addEventListener("click", function () {
     main.classList.toggle("dark-mode");
   });
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
